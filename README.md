b4c46d9d7dced8c53d67223cf199071e66a481a9
Can Demircan <cdemircan@tutanota.com>
2023-12-15 18:34:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# baklavacutter
+
+A cookiecutter template for my research projects.
+
+Run the following command to create a new project:
+```bash
+cookiecutter gh:candemircan/baklavacutter
+```
---
+++ {{cookiecutter.directory_name}}/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="{{cookiecutter.directory_name}}",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines(),
+    extras_require={
+        'dev': open("requirements-dev.txt").readlines()
+    }
+)
