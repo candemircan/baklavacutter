eec253b9fa1e84e06d4542b3a31386ec74bb60a4
Can Demircan <cdemircan@tutanota.com>
2025-03-03 09:10:34 +0100

    fix pre-commit

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@
 git init
 uv venv .{{cookiecutter.directory_name}} -p python3.12
 source .{{cookiecutter.directory_name}}/bin/activate
-uv pip install -e .
+uv pip install -e .[dev] --no-cache
 
 pre-commit install
 pre-commit run --all-files
