a71144944a40bfc76ea4cf4de45cf5b1c26f79b2
Can Demircan <cdemircan@tutanota.com>
2025-03-16 14:21:55 +0100

    dir name

--- {{cookiecutter.directory_name}}/make_disposable_env.sh
+++ {{cookiecutter.directory_name}}/make_disposable_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export TMPDIR=/localscratch/
 cd /localscratch/
-uv venv .{{cookiecutter.project_name}} -p python3.12
-source .{{cookiecutter.project_name}}/bin/activate
+uv venv .{{cookiecutter.directory_name}} -p python3.12
+source .{{cookiecutter.directory_name}}/bin/activate
 uv pip install --upgrade pip
-cd /lustre/groups/hcai/workspace/can.demircan/{{cookiecutter.project_name}}
+cd /lustre/groups/hcai/workspace/can.demircan/{{cookiecutter.directory_name}}
 uv pip install -e . --no-cache
 pre-commit install
