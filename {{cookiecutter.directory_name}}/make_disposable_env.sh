311955d0b0d002977f774152f51214c3a390b48c
Can Demircan <cdemircan@tutanota.com>
2025-03-18 14:50:52 +0100

    Update make_disposable_env.sh

--- {{cookiecutter.directory_name}}/make_disposable_env.sh
+++ {{cookiecutter.directory_name}}/make_disposable_env.sh
@@ -5,5 +5,5 @@ uv venv .{{cookiecutter.directory_name}} -p python3.12
 source .{{cookiecutter.directory_name}}/bin/activate
 uv pip install --upgrade pip
 cd /lustre/groups/hcai/workspace/can.demircan/{{cookiecutter.directory_name}}
-uv pip install -e . --no-cache
+uv pip install -e '.[dev]'
 pre-commit install
